72ab7c66e4735eeedb7b1727a9c8145fc4309a14
Hanck1215 <st11025212@gmail.com>
2023-07-05 02:01:03 +0800

    import相關的套件

--- Hello/src/main/java/Hello.java
+++ Hello/src/main/java/Hello.java
@@ -1,5 +1,13 @@
 package main.java;
+import org.springframework.boot.SpringApplication ;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration ;
+import org.springframework.web.bind.annotation.RestController ;
+import org.springframework.web.bind.annotation.RequestMapping ;
 
+@RestController
+@EnableAutoConfiguration
 public class Hello {
-    
+    public static void main(String[] args) {
+
+    }
 }
