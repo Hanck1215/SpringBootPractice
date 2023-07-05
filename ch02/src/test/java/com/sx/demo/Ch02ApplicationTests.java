a7f06af881af3c039d8f1c7b9389ac365490c300
Hanck1215 <st11025212@gmail.com>
2023-07-05 14:46:54 +0800

    新增ch02專案

---
+++ ch02/src/test/java/com/sx/demo/Ch02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sx.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
