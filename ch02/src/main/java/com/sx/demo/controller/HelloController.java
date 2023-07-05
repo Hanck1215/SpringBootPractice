135f234c1c6fb4e93411b5b532e7c1735002dc26
Hanck1215 <st11025212@gmail.com>
2023-07-05 15:04:31 +0800

    在Hello controller為類別加上annotations

--- ch02/src/main/java/com/sx/demo/controller/HelloController.java
+++ ch02/src/main/java/com/sx/demo/controller/HelloController.java
@@ -3,6 +3,11 @@ package com.sx.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
+	@RequestMapping("/")
+	String home() {
+		return "Hello world" ;
+	}
 
 }
