c7fe224a91714f33775699479cf7b54e9dc09b6b
Hanck1215 <st11025212@gmail.com>
2023-07-05 15:02:49 +0800

    在Hello controller引入兩個annotation

--- ch02/src/main/java/com/sx/demo/controller/HelloController.java
+++ ch02/src/main/java/com/sx/demo/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.sx.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class HelloController {
 
 }
