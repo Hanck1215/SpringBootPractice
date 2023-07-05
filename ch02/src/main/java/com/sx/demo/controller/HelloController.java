a008dabc68c8bfa9eced1353f792e86259653430
Hanck1215 <st11025212@gmail.com>
2023-07-05 17:55:09 +0800

    成功以https連上伺服器

--- ch02/src/main/java/com/sx/demo/controller/HelloController.java
+++ ch02/src/main/java/com/sx/demo/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	@RequestMapping("/api")
+	@RequestMapping("/")
 	String home() {
 		return "Hello" ;
 	}
