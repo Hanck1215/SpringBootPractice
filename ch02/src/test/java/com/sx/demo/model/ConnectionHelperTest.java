f7ebcead266b43f270d30f0961a0b19f02fa411a
Hanck1215 <st11025212@gmail.com>
2023-07-06 14:57:01 +0800

    創建ConnectionHelperTest.java

---
+++ ch02/src/test/java/com/sx/demo/model/ConnectionHelperTest.java
@@ -0,0 +1,9 @@
+package com.sx.demo.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class ConnectionHelperTest {
+
+}
