1ce3513c535f2953455c39ea3079e5971d3f0811
Hanck1215 <st11025212@gmail.com>
2023-07-06 14:05:36 +0800

    將jdbc的值注入到私有成員

--- ch02/src/main/java/com/sx/demo/model/ConnectionHelper.java
+++ ch02/src/main/java/com/sx/demo/model/ConnectionHelper.java
@@ -3,6 +3,13 @@ package com.sx.demo.model;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Component
 public class ConnectionHelper {
-
+	@Value("${jdbc.username}")
+	private String username ;
+	@Value("${jdbc.password}")
+	private String password ;
+	
+	
+	
 }
