8484c7ed911e940dc33f7ffa6025b48c9ff1003b
Hanck1215 <st11025212@gmail.com>
2023-07-06 15:22:39 +0800

    在ConnectionHelperTest.java寫測試

--- ch02/src/test/java/com/sx/demo/model/ConnectionHelperTest.java
+++ ch02/src/test/java/com/sx/demo/model/ConnectionHelperTest.java
@@ -1,9 +1,16 @@
 package com.sx.demo.model;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class ConnectionHelperTest {
-
+	@Autowired
+	private ConnectionHelper connHelper ;
+	@Test
+	void test() {
+		System.out.println(connHelper.getUsername()) ;
+		System.out.println(connHelper.getPassword()) ;
+	}
 }
