71bda970e1dfa93915ca2c7db485db609ce3f9b1
Hanck1215 <st11025212@gmail.com>
2023-07-07 19:18:29 +0800

    成功測試接收表單的功能

--- ch03/src/main/java/com/sun/ch03/controller/DemoController.java
+++ ch03/src/main/java/com/sun/ch03/controller/DemoController.java
@@ -1,10 +1,18 @@
 package com.sun.ch03.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping ;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController ;
 
 @RestController
 @RequestMapping("/handle")
 public class DemoController {
-
+	
+	@RequestMapping		//不含元素的@RequestMapping注釋將映射到空白路徑。等價於@@RequestMapping("")
+	public String handle(@RequestParam(name = "username", defaultValue = "anonymous") String name,
+			             @RequestParam(name = "password") String password) {
+		System.out.println(name) ;
+		System.out.println(password) ;
+		return "Finish" ;
+	}
 }
