a197efc2ba9dd0ce4f1153466b2e5997c4ed008c
Hanck1215 <st11025212@gmail.com>
2023-07-07 22:17:23 +0800

    要測試接收的物件是否有成員便數值

--- ch03/src/main/java/com/sun/ch03/controller/DemoController.java
+++ ch03/src/main/java/com/sun/ch03/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.sun.ch03.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping ;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +14,13 @@ public class DemoController {
 	
 	@RequestMapping		//不含元素的@RequestMapping注釋將映射到空白路徑。等價於@@RequestMapping("")
 	public String handle(@RequestParam(name = "username", defaultValue = "anonymous") String name,
-			             @RequestParam(name = "password") String password) {
+			             @RequestParam(name = "password") String password,
+			             @RequestParam(name = "gender") Boolean gender,
+			             @RequestParam(name = "interests") List<String> interests) {
 		System.out.println(name) ;
 		System.out.println(password) ;
+		System.out.println(gender) ;
+		System.out.println(interests) ;
 		return "Finish" ;
 	}
 	
