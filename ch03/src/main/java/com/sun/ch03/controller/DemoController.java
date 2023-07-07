d2be0562c2ac57c53c4abb87d54307a3416a94d3
Hanck1215 <st11025212@gmail.com>
2023-07-07 18:07:09 +0800

    對Controller class 進行標註

--- ch03/src/main/java/com/sun/ch03/controller/DemoController.java
+++ ch03/src/main/java/com/sun/ch03/controller/DemoController.java
@@ -3,6 +3,8 @@ package com.sun.ch03.controller;
 import org.springframework.web.bind.annotation.RequestMapping ;
 import org.springframework.web.bind.annotation.RestController ;
 
+@RestController
+@RequestMapping("/handle")
 public class DemoController {
 
 }
