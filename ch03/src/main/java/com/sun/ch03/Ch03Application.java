0db4cb9017c33f49e8f8bc1a65978d4d70c78ca8
Hanck1215 <st11025212@gmail.com>
2023-07-10 01:36:33 +0800

    替換html檔

--- ch03/src/main/java/com/sun/ch03/Ch03Application.java
+++ ch03/src/main/java/com/sun/ch03/Ch03Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ch03Application {
-	public static void main(String[] args) {//
+	public static void main(String[] args) {
 		SpringApplication.run(Ch03Application.class, args);
 	}
 
