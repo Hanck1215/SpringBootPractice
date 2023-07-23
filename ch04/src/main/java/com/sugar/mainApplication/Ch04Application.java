9c5a5e2e7b602243d85beacdca68a4e977305fe5
Hanck1215 <st11025212@gmail.com>
2023-07-23 17:33:36 +0800

    在application.properties設定檔中加入設定項:spring.thymeleaf.cache=false

--- ch04/src/main/java/com/sugar/mainApplication/Ch04Application.java
+++ ch04/src/main/java/com/sugar/mainApplication/Ch04Application.java
@@ -2,6 +2,7 @@ package com.sugar.mainApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 
 @SpringBootApplication
 public class Ch04Application {
